d9500d32ad6dbf42aa2d0a862afc9f26dea55d9d
Meliwi <melissa.gonzalez@correounivalle.edu.co>
2020-01-25 14:35:53 -0500

    Aprendiendo acerca de actualizaciones automáticas

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -6,6 +6,7 @@ import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 import PageLoading from '../components/PageLoading';
 import PageError from '../components/PageError';
+import MiniLoader from '../components/MiniLoader';
 import api from '../api';
 
 class Badges extends React.Component {
@@ -17,6 +18,13 @@ class Badges extends React.Component {
 
   componentDidMount() {
     this.fetchData();
+
+    //Declarar un intervalo para la actualizacion automática
+    this.intervalId = setInterval(this.fetchData, 5000);
+  }
+
+  componentWillMount(){
+    clearInterval(this.intervalId);
   }
 
   fetchData = async () => {
@@ -31,7 +39,7 @@ class Badges extends React.Component {
   };
 
   render() {
-    if (this.state.loading === true) {
+    if (this.state.loading === true && this.state.data === undefined) {
      return <PageLoading/>;
     }
 
@@ -61,6 +69,7 @@ class Badges extends React.Component {
           </div>
 
           <BadgesList badges={this.state.data} />
+          {this.state.loading && <MiniLoader/>}
         </div>
       </React.Fragment>
     );
